f85493971926cf9e7f7967a656bc6eec13581819
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-01-15 18:02:43 +0100

    Sur la première ou la dernière ligne de la grille, les tirets disparaissent au fur et à mesure qu'elles se remplissent

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ grille = [[0,0,0,0,0,0],
           [0,0,0,0,0,0],
           [0,0,0,0,0,0],
           [0,0,0,0,0,0],
-          [0,0,0,0,0,0]] # Grille du jeu. Ici, une case ayant pour valeur 0 veut dire qu'elle est vide. Si elle a pour valeur 1, alors le joueur 1 y a placé un pion, et si elle a pour valeur 2, alors le joueur 2 y a placé un pion.
+          [0,0,0,2,1,0]] # Grille du jeu. Ici, une case ayant pour valeur 0 veut dire qu'elle est vide. Si elle a pour valeur 1, alors le joueur 1 y a placé un pion, et si elle a pour valeur 2, alors le joueur 2 y a placé un pion.
           
 
 def recupererSymbole(joueur):
@@ -21,16 +21,29 @@ def recupererSymbole(joueur):
 def afficherGrille():
     "Afficher la grille du jeu"
     for ligne in range(len(grille)): # Pour chaque ligne de la grille
-        for colonne in grille[ligne]: # Pour chaque colonne de la ligne actuelle
+        for colonne in grille[ligne] : # Pour chaque colonne de la ligne actuelle
+            #print("Contenu de la case :", colonne)
             if ligne == 0 or ligne == 5: # Si on est à la première ou à la dernière ligne de la grille
-                print("-", end = " ") 
-
+                if colonne == 0:
+                    print("-", end = " ") 
             
 
-            if ligne > 0 and ligne < 5: # Tant qu'on est ni à la première ni à la dernière ligne
-                print("|", end =" ")
+            else:
+                print("|", end = " ")
+
+
+            if colonne != 0: # Si une colonne a une case dont la valeur est différente de 0, alors c'est qu'un joueur y a placé un pion
+                    #print("Le contenu de la case vaut", colonne)
+                    print(recupererSymbole(colonne), end= " ") # On affiche le symbole du joueur dans la grille de jeu
+
+
+
+                
+                    
+                   
 
         print() # On appelle print() pour faire un saut à la ligne
+
 
 
 
